2dd98c92a657f77d67dcfb4b36a30d6cf61c7b49
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-11 13:22:16 +0200

    añadido skeleton y extraida la ui para mostra la info detalle

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import {
-  VStack,
-  Heading,
-  Box,
-  Image,
-  Text,
-  HStack,
-} from '@chakra-ui/react'
 import { useState, useEffect } from 'react'
 import { getRandomBeer } from '../services/getRandomBeer'
-import BeerList from './BeerList'
+import SingleBeer from './SingleBeer'
+import Skeleton from './Skeleton'
 
 export function RandomBeer() {
   const [randomBeer, setRandomBeer] = useState()
@@ -19,23 +12,9 @@ export function RandomBeer() {
   }, [])
 
   return (
-    randomBeer && (
-      <HStack align="start" mt="10">
-        <VStack align="start" width="400px">
-          <Heading textAlign="left">{randomBeer.name}</Heading>
-          <Text fontSize="md">{randomBeer.description}</Text>
-        </VStack>
-        <Box boxSize="lg">
-          {randomBeer.image_url && (
-            <Image
-              boxSize="300px"
-              objectFit="contain"
-              src={randomBeer.image_url}
-              alt={randomBeer.name}
-            />
-          )}
-        </Box>
-      </HStack>
-    )
+    <>
+      {!randomBeer && <Skeleton />}
+      {randomBeer && <SingleBeer beerData={randomBeer} />}
+    </>
   )
 }
