74b0c90f5ddc7b16e623f598c69f5fbad32e18b9
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-11 13:21:26 +0200

    añadida info detalle y skeleton on load

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -1,12 +1,18 @@
-import { Box } from '@chakra-ui/react'
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { getBeer } from '../services/getBeer'
+import SingleBeer from './SingleBeer'
+import Skeleton from './Skeleton'
 
 export default function BeerDetail({ id }) {
   const [beerDetail, setBeerDetail] = useState()
   useEffect(() => {
     getBeer(id).then(setBeerDetail)
   }, [])
-  return <>{beerDetail && <Box>{beerDetail.name}</Box>}</>
+  return (
+    <>
+      {!beerDetail && <Skeleton />}
+      {beerDetail && <SingleBeer beerData={beerDetail} />}
+    </>
+  )
 }
