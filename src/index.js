c566c054984940fea211999619a14cf9a6ad75f3
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-11 13:20:55 +0200

    React Strict Mode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 
 root.render(
+  //<React.StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  //</React.StrictMode>
 )
